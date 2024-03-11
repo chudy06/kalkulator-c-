48f00baa37098a44b6af2c5c94ab42df86a73ecd
chudy06 <163013205+chudy06@users.noreply.github.com>
2024-03-11 15:49:41 +0100

    just calculator

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    float liczba1, liczba2;
+    char operacja;
+
+    cout << "Autor: Kamil Cichecki 3TI " << endl;
+
+     cout << "\n Podaj pierwsza liczbe: ";
+     cin >> liczba1;
+
+     cout << "\n Podaj operacje(+, -, *, /): ";
+     cin >> operacja;
+
+     cout << "\n Podaj druga liczbe: ";
+     cin >> liczba2;
+
+     if  (operacja == '+') {
+        cout << "Wynik: " << liczba1 + liczba2 << endl;
+     } else if (operacja == '-') {
+        cout << "Wynik: " << liczba1 - liczba2 << endl;
+     } else if (operacja == '*') {
+        cout << "Wynik: " << liczba1 * liczba2 << endl;
+     } else if (operacja == '/') {
+        if (liczba2 != 0) {
+            cout << "\n Wynik: " << liczba1 / liczba2 << endl;
+        } else {
+            cout << "\n Nie mozna dzielic przez zero :) " << endl;
+        }
+     } else {
+        cout << "\n Nie prawidlowa operacja" << endl;
+     }
+
+     return 0;
+}
